cbd034678ba60d8ee09eb2984ec5f3ce717bbc2f
570468837 <570468837@qq.com>
2015-07-11 00:45:48 +0800

    new

--- src/presentation/PanelForInfo.java
+++ src/presentation/PanelForInfo.java
@@ -5,12 +5,12 @@ import java.awt.Color;
 import javax.swing.*;
 
 public class PanelForInfo extends JPanel{
-	public PanelForInfo(MainFrame mf){
+	public PanelForInfo(Panel p){
 		this.setSize(800,520);
 		this.setLocation(0,120);
 		this.setVisible(false);
-		this.setOpaque(false);
-		this.setBackground(Color.pink);
-		mf.getContentPane().add(this);
+		this.setOpaque(true);
+		this.setBackground(Color.LIGHT_GRAY);
+		p.add(this);
 	}
 }
