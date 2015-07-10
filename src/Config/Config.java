eabe94bc65267b3bc34722613ee2a144ccf8cbe2
570468837 <570468837@qq.com>
2015-07-11 01:17:02 +0800

    增加了两个类

---
+++ src/Config/Config.java
@@ -0,0 +1,10 @@
+/**
+ * 这个类用来存放一下全局变量
+ */
+package Config;
+
+public class Config {
+	public static final String[] tastes = {"All","西餐","中餐"};
+	
+	
+}
---
+++ src/Test/InfoTest.java
@@ -0,0 +1,21 @@
+package Test;
+
+import presentation.MainFrame;
+import presentation.Panel;
+import presentation.PanelForInfo;
+import presentation.PanelForTitle;
+
+public class InfoTest {
+	static MainFrame mf;
+	static PanelForTitle pft;
+	static Panel p;
+	static PanelForInfo pfi;
+	public static void main(String[] args){
+		mf=new MainFrame();
+		p=new Panel(mf);
+		pft=new PanelForTitle(p);
+		pft.setVisible(true);
+		pfi = new PanelForInfo(p);
+		pfi.setVisible(true);
+	}
+}
